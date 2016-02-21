5dd48115323cfe028206b2e8c19a01b726639ffb
decker <adecker5@ycp.edu>
2016-02-21 18:57:42 -0500

    Fixed project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
-(defproject ascii-never-dies
+(defproject ascii-never-dies "1.0.0"
+  :description "Web-based rogue-lite written in Clojure"
   :url "https://github.com/seniordesign2/game-client"
   :min-lein-version "2.6.1"
-  :dependencies '[
-                  [org.clojure/clojure "1.7.0"]
-                  [org.clojure/clojurescript "1.7.170"]
-                  [org.clojure/java.jdbc "0.4.2"]
-                  [mysql/mysql-connector-java "5.1.38"]
-                  ]
+  :dependencies [
+                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [compojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [environ "1.0.0"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc4"]
+                ]
   :source-paths #{"src/cljs"}
   :resource-paths #{"html"})
