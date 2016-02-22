95842e82a1b64a93bc04103805f49f07c92f6f11
decker <adecker5@ycp.edu>
2016-02-21 20:15:48 -0500

    local change viewing WORKS

--- src/ascii_never_dies/handler.clj
+++ src/ascii_never_dies/handler.clj
@@ -1,11 +1,24 @@
 (ns ascii-never-dies.handler
   (:require [compojure.core :refer :all]
+            [compojure.handler :refer [site]]
             [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (pr-str ["Hello World"])})
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" []
+       (splash))
   (route/not-found "Not found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
